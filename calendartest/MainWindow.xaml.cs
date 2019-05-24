33834396be434ba86c9d4a6246b1768e464f327e
sideup66 <info@getontrak.net>
2019-05-23 22:44:15 -0400

    added comments and a functional refresh ability

--- calendartest/MainWindow.xaml.cs
+++ calendartest/MainWindow.xaml.cs
@@ -39,11 +39,14 @@ namespace calendartest
             {
                 //create a new item
                 addentry frm2 = new addentry();
-                frm2.Show();
                 using (StreamWriter day = new StreamWriter("day.txt"))
                 {
                     day.WriteLine("Sunday");
                 }
+                frm2.ShowDialog();
+                //refresh the calendar when we are done
+                calendarcleanup();
+                refreshcalendar();
             }
         }
 
@@ -58,11 +61,14 @@ namespace calendartest
             {
                 //create a new item
                 addentry frm2 = new addentry();
-                frm2.Show();
                 using (StreamWriter day = new StreamWriter("day.txt"))
                 {
                     day.WriteLine("Monday");
                 }
+                frm2.ShowDialog();
+                //cleanup and refresh the calendar
+                calendarcleanup();
+                refreshcalendar();
             }
         }
         private void tuelist_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -76,11 +82,14 @@ namespace calendartest
             {
                 //create a new item
                 addentry frm2 = new addentry();
-                frm2.Show();
                 using (StreamWriter day = new StreamWriter("day.txt"))
                 {
                     day.WriteLine("Tuesday");
                 }
+                frm2.Show();
+                //cleanup and refresh the calendar
+                calendarcleanup();
+                refreshcalendar();
             }
         }
         private void wedlist_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -94,11 +103,14 @@ namespace calendartest
             {
                 //create a new item
                 addentry frm2 = new addentry();
-                frm2.Show();
                 using (StreamWriter day = new StreamWriter("day.txt"))
                 {
                     day.WriteLine("Wednesday");
                 }
+                frm2.ShowDialog();
+                //cleanup and refresh the calendar
+                calendarcleanup();
+                refreshcalendar();
             }
         }
         private void thurlist_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -112,11 +124,14 @@ namespace calendartest
             {
                 //create a new item
                 addentry frm2 = new addentry();
-                frm2.Show();
                 using (StreamWriter day = new StreamWriter("day.txt"))
                 {
                     day.WriteLine("Thursday");
                 }
+                frm2.ShowDialog();
+                //cleanup and refresh the calendar
+                calendarcleanup();
+                refreshcalendar();
             }
         }
         private void frilist_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -130,11 +145,15 @@ namespace calendartest
             {
                 //create a new item
                 addentry frm2 = new addentry();
-                frm2.Show();
                 using (StreamWriter day = new StreamWriter("day.txt"))
                 {
                     day.WriteLine("Friday");
                 }
+                frm2.ShowDialog();
+                //cleanup and refresh the calendar
+                calendarcleanup();
+                refreshcalendar();
+
             }
         }
         private void satlist_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -148,20 +167,43 @@ namespace calendartest
             {
                 //create a new item
                 addentry frm2 = new addentry();
-                frm2.Show();
                 using (StreamWriter day = new StreamWriter("day.txt"))
                 {
                     day.WriteLine("Saturday");
                 }
+                frm2.ShowDialog();
+                //cleanup and refresh the calendar
+                calendarcleanup();
+                refreshcalendar();
             }
         }
+
+        private void calendarcleanup()
+        {
+            //here we will be cleaning up the calendar by deleteing old entries (any change triggers a refresh and will call this method to cleanup first)
+            monlist.Items.Clear();
+            monlist.Items.Add("Click here");
+            tuelist.Items.Clear();
+            tuelist.Items.Add("Click here");
+            wedlist.Items.Clear();
+            wedlist.Items.Add("Click here");
+            thurlist.Items.Clear();
+            thurlist.Items.Add("Click here");
+            frilist.Items.Clear();
+            frilist.Items.Add("Click here");
+            satlist.Items.Clear();
+            satlist.Items.Add("Click here");
+            sunlist.Items.Clear();
+            sunlist.Items.Add("Click here");
+        }
+
         private void Cleanupdb()
         {
             using (SQLiteConnection dbconnection = new SQLiteConnection("DataSource=calendardb.db;Version=3;"))
             {
                 dbconnection.Open();
 
-                //TODO: here we will be cleaning up old entries in the db
+                //this logic will clean up old entries that are before the current date
                 string sql = "select * from single_events order by Creation_Date";
                 SQLiteCommand command = new SQLiteCommand(sql, dbconnection);
                 SQLiteDataReader reader = command.ExecuteReader();
@@ -186,11 +228,52 @@ namespace calendartest
             using (SQLiteConnection dbconnection = new SQLiteConnection("DataSource=calendardb.db;Version=3;"))
             {
                 dbconnection.Open();
-                //read in everything for the calendar
+                //read in everything from the calendar and put it in the 
                 string sql = "select * from single_events order by Creation_Date";
-                SQLiteCommand command = new SQLiteCommand(sql, dbconnection);
-                //here we write everything to the calendar
 
+                SQLiteCommand command = new SQLiteCommand(sql, dbconnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+                string dbdate;
+                DateTime daytoadd;
+                string dayofweek;
+                while (reader.Read())
+                {
+                    //populate single events
+                    dbdate = reader["Creation_Date"].ToString();
+                    daytoadd = Convert.ToDateTime(dbdate);
+                    dayofweek = daytoadd.ToString("dddd");
+                    if (dayofweek.Contains("Sunday"))
+                    {
+                        //add the value to the sunday list
+                        sunlist.Items.Add(reader["Event_Name"]);
+                    }
+                    else if (dayofweek.Contains("Monday"))
+                    {
+                        monlist.Items.Add(reader["Event_Name"]);
+
+                    }
+                    else if (dayofweek.Contains("Tuesday"))
+                    {
+                        tuelist.Items.Add(reader["Event_Name"]);
+
+                    }
+                    else if (dayofweek.Contains("Wednesday"))
+                    {
+                        wedlist.Items.Add(reader["Event_Name"]);
+                    }
+                    else if (dayofweek.Contains("Thursday"))
+                    {
+                        thurlist.Items.Add(reader["Event_Name"]);
+                    }
+                    else if (dayofweek.Contains("Friday"))
+                    {
+                        frilist.Items.Add(reader["Event_Name"]);
+                    }
+                    else if (dayofweek.Contains("Saturday"))
+                    {
+                        satlist.Items.Add(reader["Event_Name"]);
+                    }
+                }
             }
         }
 
